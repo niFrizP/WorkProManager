01a3f3767bf65691bf6611d76e0e31f69173f71f
Иiкo FP <102751106+niFrizP@users.noreply.github.com>
2024-10-07 23:14:22 -0300

    Actualizar barra lateral y componentes de inicio de sesión

---
+++ WorkProManager/FRONT/src/app/new-ot/new-ot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-ot',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-ot.component.html',
+  styleUrl: './new-ot.component.css'
+})
+export class NewOtComponent {
+
+}
