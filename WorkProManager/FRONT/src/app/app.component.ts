f1a9b7a49ae9e055a1c475bd89bdd53168d271dd
matilinucs <133159641+matilinucs@users.noreply.github.com>
2024-10-12 01:04:13 -0300

    backend 3

--- WorkProManager/FRONT/src/app/app.component.ts
+++ WorkProManager/FRONT/src/app/app.component.ts
@@ -3,19 +3,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClienteService } from './services/cliente.service';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { OrdersModule } from './pages/orders/orders.module';
+import { ModuleWithProviders } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, CommonModule, OrdersModule, SidebarComponent, NgxPaginationModule ,ReactiveFormsModule, FormsModule, HttpClientModule, ToastrModule],
-=======
   imports: [
     RouterOutlet,
     CommonModule,
@@ -24,8 +20,7 @@ import { OrdersModule } from './pages/orders/orders.module';
     FormsModule,
     HeaderComponent,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()],
->>>>>>> c59ed2432fa62960f41854a4b1d314023022219c
+    ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
