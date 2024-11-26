e9afd8d238fdb7fc96e4b417f2dc69ab9069dad2
enfaki <ba.olivares@duocuc.cl>
2024-11-26 13:51:07 -0300

    a

--- WorkProManager/BACK/dist/db/connection.js
+++ WorkProManager/BACK/dist/db/connection.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('ot_db', 'root', '7697', {
+const sequelize = new sequelize_1.Sequelize('ot_db', 'root', 'Mugiwara1$', {
     host: 'localhost',
     dialect: 'mysql',
 });
