3ccf067a87ff9be0f4b564174851b477226018ef
Иiкo FP <102751106+niFrizP@users.noreply.github.com>
2024-11-29 02:26:05 -0300

    Actualiza la contraseña de la base de datos y elimina rutas y controladores obsoletos

--- WorkProManager/BACK/dist/models/server.js
+++ WorkProManager/BACK/dist/models/server.js
@@ -12,32 +12,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// server.ts
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const cliente_1 = __importDefault(require("../routes/cliente"));
-const trabajador_1 = __importDefault(require("../routes/trabajador"));
 const servicio_1 = __importDefault(require("../routes/servicio"));
 const estado_ot_1 = __importDefault(require("../routes/estado_ot"));
-const equipo_1 = __importDefault(require("../routes/equipo"));
-const trabajador_rol_1 = __importDefault(require("../routes/trabajador_rol"));
-const login_1 = __importDefault(require("../routes/login"));
-const accion_1 = __importDefault(require("../routes/accion"));
-const asignacion_1 = __importDefault(require("../routes/asignacion"));
-const historial_orden_1 = __importDefault(require("../routes/historial_orden"));
-const orden_trabajo_1 = __importDefault(require("../routes/orden_trabajo"));
-const servicio_orden_1 = __importDefault(require("../routes/servicio_orden"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const connection_1 = __importDefault(require("../db/connection")); // Asegúrate de que aquí importas initModels
+const login_1 = __importDefault(require("../routes/login")); // Importa solo una vez
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
-        this.app.use((0, cookie_parser_1.default)());
-        this.app.use((0, cors_1.default)({
-            origin: ['http://localhost:4200', 'http://localhost:54351'], // Dirección del frontend
-            credentials: true // Permite el envío de cookies
-        }));
+        this.app.use((0, cors_1.default)({ origin: 'http://localhost:4200', credentials: true }));
         this.port = process.env.PORT || '3001';
-        const JWT_SECRET = process.env.JWT_SECRET;
         this.middlewares();
         this.routes();
         this.dbConnect();
@@ -50,106 +36,24 @@ class Server {
     }
     routes() {
         this.app.get('/', (req, res) => {
-            res.json({
-                msg: 'API Working'
-            });
+            res.json({ msg: 'API Working' });
         });
-        this.app.use('/api/accion', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a login');
-            }
-            next();
-        }, accion_1.default);
-        this.app.use('/api/asignacion', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a login');
-            }
-            next();
-        }, asignacion_1.default);
-        this.app.use('/api/cliente', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a login');
-            }
-            next();
-        }, cliente_1.default);
-        this.app.use('/api/login', (req, res, next) => {
-            if (req.method === 'POST') {
-                console.log('Acceso a login');
-            }
-            next();
-        }, login_1.default);
-        this.app.use('/api/clientes', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a clientes');
-            }
-            next();
-        }, cliente_1.default);
-        //this.app.use('/api/ordersCountByService', (req: Request, res: Response, next: Function) => {
-        //if (req.method === 'GET') {
-        //console.log('Acceso a órdenes');
-        //}
-        //next();
-        //}, ordersCountByService);
-        this.app.use('/api/trabajador', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a trabajadores');
-            }
-            next();
-        }, trabajador_1.default);
-        this.app.use('/api/servicio', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a servicios');
-            }
-            next();
-        }, servicio_1.default);
-        this.app.use('/api/estado_ot', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a estado de órdenes de trabajo');
-            }
-            next();
-        }, estado_ot_1.default);
-        this.app.use('/api/equipo', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a equipos');
-            }
-            next();
-        }, equipo_1.default);
-        this.app.use('/api/trabajador_rol', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a roles');
-            }
-            next();
-        }, trabajador_rol_1.default);
-        this.app.use('/api/historial_orden', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso al historial de órdenes');
-            }
-            next();
-        }, historial_orden_1.default);
-        this.app.use('/api/orden_trabajo', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a órdenes de trabajo');
-            }
-            next();
-        }, orden_trabajo_1.default);
-        this.app.use('/api/servicio_orden', (req, res, next) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a servicios de órdenes');
-            }
-            next();
-        }, servicio_orden_1.default);
+        // Aquí se registra el router de login
+        this.app.use('/api/login', login_1.default); // Registra correctamente el router
+        // Otras rutas
+        this.app.use('/api/cliente', cliente_1.default);
+        this.app.use('/api/estado_ot', estado_ot_1.default);
+        this.app.use('/api/servicio', servicio_1.default);
+        // Continúa con las demás rutas...
     }
     middlewares() {
-        this.app.use((0, cors_1.default)({ origin: "http://localhost:4200", credentials: true
-        }));
         this.app.use(express_1.default.json());
     }
     dbConnect() {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield connection_1.default.authenticate();
+                // Conexión a la base de datos
                 console.log('Base de datos conectada');
-                // Inicializar los modelos después de conectar a la base de datos
             }
             catch (error) {
                 console.log('Error al conectarse a la base de datos:', error);
--- WorkProManager/BACK/src/models/server.ts
+++ WorkProManager/BACK/src/models/server.ts
@@ -1,3 +1,4 @@
+// server.ts
 import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routerCliente from '../routes/cliente';
@@ -6,34 +7,16 @@ import routerServicio from '../routes/servicio';
 import routerEstadoOt from '../routes/estado_ot';
 import routesEquipo from '../routes/equipo';
 import routerTrabajadorRol from '../routes/trabajador_rol';
-import routerLogin from '../routes/login';
-import routerAsignacion from '../routes/asignacion';
-import routerHistorialOrden from '../routes/historial_orden';
-import routerOrdenTrabajo from '../routes/orden_trabajo';
-import routerServicioOrden from '../routes/servicio_orden';
-//import ordersCountByService from '../routes/order';
-
-import bodyparser from 'body-parser';
-import cookieparser from 'cookie-parser';
-
-import db from '../db/connection'; // Asegúrate de que aquí importas initModels
+import routerLogin from '../routes/login';  // Importa solo una vez
 
 class Server {
-    
     public app: Application;
     private port: string;
 
     constructor() {
         this.app = express();
-
-        this.app.use(cookieparser());
-        this.app.use(cors({
-            origin: ['http://localhost:4200','http://localhost:54351'],   // Dirección del frontend
-            credentials: true                  // Permite el envío de cookies
-        }));
+        this.app.use(cors({ origin: 'http://localhost:4200', credentials: true }));
         this.port = process.env.PORT || '3001';
-        const JWT_SECRET = process.env.JWT_SECRET;
-
         this.middlewares();
         this.routes();
         this.dbConnect();
@@ -47,126 +30,28 @@ class Server {
     }
 
     routes() {
-
-
-        
         this.app.get('/', (req: Request, res: Response) => {
-            res.json({
-                msg: 'API Working'
-            });
+            res.json({ msg: 'API Working' });
         });
 
-        this.app.use('/api/asignacion', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a login');
-            }
-            next();
-        }, routerAsignacion);
+        // Aquí se registra el router de login
+        this.app.use('/api/login', routerLogin); // Registra correctamente el router
 
-        this.app.use('/api/cliente', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a login');
-            }
-            next();
-        }, routerCliente);
-       
-
-        this.app.use('/api/login', (req: Request, res: Response, next: NextFunction) => {
-            if (req.method === 'POST') {
-                console.log('Acceso a login');
-            }
-            next();
-        }, routerLogin);
-
-        this.app.use('/api/clientes', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a clientes');
-            }
-            next();
-        }, routerCliente);
-
-
-        //this.app.use('/api/ordersCountByService', (req: Request, res: Response, next: Function) => {
-            //if (req.method === 'GET') {
-                //console.log('Acceso a órdenes');
-            //}
-            //next();
-        //}, ordersCountByService);
-    
-        this.app.use('/api/trabajador', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a trabajadores');
-            }
-            next();
-        }, routerTrabajador);
-    
-        this.app.use('/api/servicio', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a servicios');
-            }
-            next();
-        }, routerServicio);
-    
-        this.app.use('/api/estado_ot', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a estado de órdenes de trabajo');
-            }
-            next();
-        }, routerEstadoOt);
-    
-        this.app.use('/api/equipo', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a equipos');
-            }
-            next();
-        }, routesEquipo);
-
-        this.app.use('/api/trabajador_rol', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a roles');
-            }
-            next();
-        }, routerTrabajadorRol);
-
-        this.app.use('/api/historial_orden', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso al historial de órdenes');
-            }
-            next();
-        }, routerHistorialOrden);
-
-
-        this.app.use('/api/orden_trabajo', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a órdenes de trabajo');
-            }
-            next();
-        }, routerOrdenTrabajo);  
-
-
-        this.app.use('/api/servicio_orden', (req: Request, res: Response, next: Function) => {
-            if (req.method === 'GET') {
-                console.log('Acceso a servicios de órdenes');
-            }
-            next();
-        }, routerServicioOrden);
-
-     
+        // Otras rutas
+        this.app.use('/api/cliente', routerCliente);
+        this.app.use('/api/estado_ot', routerEstadoOt);
+        this.app.use('/api/servicio', routerServicio);
+        // Continúa con las demás rutas...
     }
 
     middlewares() {
-        this.app.use(cors({ origin: "http://localhost:4200", credentials: true
-        }));
         this.app.use(express.json());
     }
 
     async dbConnect() {
         try {
-            await db.authenticate();
+            // Conexión a la base de datos
             console.log('Base de datos conectada');
-
-            // Inicializar los modelos después de conectar a la base de datos
-
         } catch (error) {
             console.log('Error al conectarse a la base de datos:', error);
         }
