8092bab277e31c0c4b477545c432c11299e1fd9f
Иiкo FP <102751106+niFrizP@users.noreply.github.com>
2024-12-03 21:04:39 -0300

    Mejora UX ordenes y sidebar

--- WorkProManager/BACK/dist/models/server.js
+++ WorkProManager/BACK/dist/models/server.js
@@ -15,9 +15,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const http_1 = __importDefault(require("http"));
-const socket_io_1 = __importDefault(require("socket.io"));
-const lusca_1 = __importDefault(require("lusca")); // Importa lusca
+const socket_io_1 = require("socket.io");
+const lusca_1 = __importDefault(require("lusca"));
 const express_session_1 = __importDefault(require("express-session"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+// Rutas
 const trabajador_1 = __importDefault(require("../routes/trabajador"));
 const insertarCotizacion_1 = __importDefault(require("../routes/insertarCotizacion"));
 const servicio_1 = __importDefault(require("../routes/servicio"));
@@ -27,7 +29,7 @@ const insertar_servicio_orden_1 = __importDefault(require("../routes/insertar_se
 const estado_ot_1 = __importDefault(require("../routes/estado_ot"));
 const orden_trabajo_1 = __importDefault(require("../routes/orden_trabajo"));
 const servicio_orden_1 = __importDefault(require("../routes/servicio_orden"));
-// Importar los modelos
+// Modelos
 const trabajador_2 = __importDefault(require("./trabajador"));
 const trabajador_rol_1 = __importDefault(require("./trabajador_rol"));
 const asignacion_1 = __importDefault(require("./asignacion"));
@@ -40,17 +42,17 @@ const orden_trabajo_2 = __importDefault(require("./orden_trabajo"));
 const servicio_orden_2 = __importDefault(require("./servicio_orden"));
 const historial_orden_1 = __importDefault(require("./historial_orden"));
 const historial_servicio_orden_1 = __importDefault(require("./historial_servicio_orden"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const servicio_orden_3 = require("../controllers/servicio_orden"); // Importa las funciones auxiliares
+// Controladores
+const servicio_orden_3 = require("../controllers/servicio_orden");
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
         this.port = process.env.PORT || '3001';
-        // Configurar el servidor HTTP para usar con Socket.IO
+        // Configuración de HTTP y Socket.IO
         this.server = http_1.default.createServer(this.app);
-        this.io = new socket_io_1.default.Server(this.server, {
+        this.io = new socket_io_1.Server(this.server, {
             cors: {
-                origin: 'http://localhost:4200', // Asegúrate de permitir el frontend Angular
+                origin: 'http://localhost:4200', // Frontend Angular
                 methods: ['GET', 'POST'],
                 credentials: true,
             }
@@ -59,12 +61,11 @@ class Server {
         this.midlewares();
         this.routes();
         this.dbConnect();
-        // Configurar eventos de Socket.IO
         this.socketEvents();
     }
     listen() {
         this.server.listen(this.port, () => {
-            console.log('Aplicacion corriendo en el puerto ' + this.port);
+            console.log('Aplicación corriendo en el puerto ' + this.port);
         });
     }
     routes() {
@@ -81,14 +82,13 @@ class Server {
     midlewares() {
         // Configuración de CORS
         this.app.use((0, cors_1.default)({
-            origin: 'http://localhost:3001', // Reemplaza con la URL de tu frontend
-            credentials: true, // Permitir el envío de cookies y encabezados de autorización
+            origin: 'http://localhost:4200', // Reemplaza con la URL de tu frontend
+            credentials: true,
         }));
-        // Parseo del body
+        // Parseo de body y cookies
         this.app.use(express_1.default.json());
-        // Analizar cookies
         this.app.use((0, cookie_parser_1.default)());
-        // Configurar sesión (debe ir antes de lusca)
+        // Configurar sesión
         this.app.use((0, express_session_1.default)({
             secret: '123', // Cambia esto por una clave secreta segura
             resave: false,
@@ -98,53 +98,47 @@ class Server {
                 httpOnly: true
             }
         }));
-        // Opción 1: Deshabilitar CSRF temporalmente y mantener otras protecciones
+        // Protección con Lusca
         this.app.use((0, lusca_1.default)({
             xframe: 'SAMEORIGIN',
             xssProtection: true
         }));
-        // Opción 2: O si prefieres mantener CSRF pero con configuración más permisiva
-        /*
-        this.app.use(lusca({
-            csrf: {
-                angular: true // Esto es específico para aplicaciones Angular
-            },
-            xframe: 'SAMEORIGIN',
-            xssProtection: true
-        }));
-        */
     }
     dbConnect() {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield trabajador_2.default.sync();
-                yield trabajador_rol_1.default.sync();
-                yield asignacion_1.default.sync();
-                yield cliente_1.default.sync();
-                yield marca_2.default.sync();
-                yield equipo_2.default.sync();
-                yield estado_ot_2.default.sync();
-                yield servicio_2.default.sync();
-                yield orden_trabajo_2.default.sync();
-                yield servicio_orden_2.default.sync();
-                yield historial_orden_1.default.sync();
-                yield historial_servicio_orden_1.default.sync();
+                // Sincronización de modelos
+                yield Promise.all([
+                    trabajador_2.default.sync(),
+                    trabajador_rol_1.default.sync(),
+                    asignacion_1.default.sync(),
+                    cliente_1.default.sync(),
+                    marca_2.default.sync(),
+                    equipo_2.default.sync(),
+                    estado_ot_2.default.sync(),
+                    servicio_2.default.sync(),
+                    orden_trabajo_2.default.sync(),
+                    servicio_orden_2.default.sync(),
+                    historial_orden_1.default.sync(),
+                    historial_servicio_orden_1.default.sync()
+                ]);
+                console.log('Conexión a la base de datos exitosa');
             }
             catch (error) {
-                console.error('Unable to connect to the database:', error);
+                console.error('Error al conectar a la base de datos:', error);
             }
         });
     }
     socketEvents() {
         this.io.on('connection', (socket) => {
             console.log('Un cliente se ha conectado', socket.id);
-            // Enviar un mensaje de bienvenida al cliente
+            // Evento de bienvenida
             socket.emit('mensaje', '¡Hola desde el servidor!');
-            // Recibir mensaje desde el cliente
+            // Escuchar mensaje desde el cliente
             socket.on('mensajeCliente', (data) => {
                 console.log('Mensaje recibido del cliente:', data);
             });
-            // Evento para obtener servicios habilitados
+            // Obtener servicios habilitados
             socket.on('getServiciosHabilitados', (id_ot) => __awaiter(this, void 0, void 0, function* () {
                 try {
                     const serviciosHabilitados = yield (0, servicio_orden_3.fetchServiciosHabilitados)(id_ot);
@@ -160,7 +154,7 @@ class Server {
                     console.error(error);
                 }
             }));
-            // Evento para obtener servicios deshabilitados
+            // Obtener servicios deshabilitados
             socket.on('getServiciosDeshabilitados', (id_ot) => __awaiter(this, void 0, void 0, function* () {
                 try {
                     const serviciosDeshabilitados = yield (0, servicio_orden_3.fetchServiciosDeshabilitados)(id_ot);
@@ -176,7 +170,7 @@ class Server {
                     console.error(error);
                 }
             }));
-            // Evento cuando un cliente se desconecta
+            // Desconexión del cliente
             socket.on('disconnect', () => {
                 console.log('Un cliente se ha desconectado', socket.id);
             });
